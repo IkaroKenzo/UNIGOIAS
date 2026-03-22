d190cb75591559622f7542f1f61ec83ff12121e4
IkaroKenzo <ikarodunop@gmail.com>
2026-03-22 12:12:40 -0300

    Exercise 3 in C

---
+++ Lista1/exercise3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+int a = 7;
+int b = 8;
+int c = 9;
+
+scanf("%i, %i, %i", &a, &b, &c);
+
+int result = (a + b + c) / 3;
+
+printf("The arithmetic mean is: %i\n", result);
+
+    return 0;
+
+
+}
