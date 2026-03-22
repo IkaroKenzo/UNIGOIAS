0ab47aec20250c430561eef609b8c8ea4291efef
IkaroKenzo <ikarodunop@gmail.com>
2026-03-22 20:50:09 -0300

    exercise 8 in C

---
+++ Lista1/exercise8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y, product;
+    scanf("%i", &x);
+    scanf("%i", &y);
+    product = x * y;
+
+    printf("%i\n", product);
+    
+    return 0;
+}
