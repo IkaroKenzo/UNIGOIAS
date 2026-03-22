2ed91116ebb62aeb26442009a575ca7ba33aa9f0
IkaroKenzo <ikarodunop@gmail.com>
2026-03-22 12:21:58 -0300

    exercise 4 in C

---
+++ Lista1/exercise4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int x, y;
+    scanf("%i", &x);
+    scanf("%i", &y);
+ 
+    printf("The numbers typed were: %i and %i\n", x, y);
+    
+
+    return 0;
+}
