3133bc0cebf09711fc2e80211e10102a7ec4b60e
IkaroKenzo <ikarodunop@gmail.com>
2026-04-06 16:23:22 -0300

    exercise 13 (math) in C

---
+++ Lista2/exercise13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double a = 5, b = 7, angle = 45;
+    double rad, c;
+
+    rad = angulo * 3.14 / 180.0;
+
+    c = sqrt(a*a + b*b - 2*a*b*cos(rad));
+
+    printf("c = %.2f\n", c);
+
+    return 0;
+}
