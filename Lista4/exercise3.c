1fb31b35bda077e06b5d0338a786c19c285085c7
IkaroKenzo <ikarodunop@gmail.com>
2026-05-15 13:58:03 -0300

    Loops in C

---
+++ Lista4/exercise3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Escreva um algoritmo que imprima todos os números inteiros de 100 a 1 (em ordem decrescente)
+
+int main(){
+	
+	int n;
+	for(n = 100; n > 0; n--){
+		printf("%i\n", n);
+	}	
+	return 0;
+}
