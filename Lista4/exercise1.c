4a81daed2d724feb07a6ac9c6c555d72679e307f
IkaroKenzo <ikarodunop@gmail.com>
2026-05-15 13:41:53 -0300

    Loops in C

---
+++ Lista4/exercise1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Escreva um algoritmo que imprima todos os números inteiros de 0 a 50.
+
+int main(){
+	
+	int n = 0;
+	
+	do{
+		printf("%i\n", n);
+		n++;
+	}while(n < 51);
+		
+	return 0;
+}
