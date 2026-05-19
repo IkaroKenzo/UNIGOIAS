9d7e8058b9d641d9de5875db7e6c3c45436e2ee5
IkaroKenzo <ikarodunop@gmail.com>
2026-05-18 22:49:40 -0300

    Loops in C

---
+++ Lista4/exercise4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Escreva um algoritmo que imprima todos os números inteiros de 100 a 200.
+
+int main(){
+	int i;
+	for(i = 100; i <= 200; i++){
+		printf("%i\n", i);
+	}
+	return 0;
+}
